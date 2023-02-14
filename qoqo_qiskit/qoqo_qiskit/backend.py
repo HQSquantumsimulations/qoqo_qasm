c2f1354b440a9b0e5c60c71e69f3a5a2dc7d60e8
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-02-14 16:00:59 +0000

    small restructure + two Pragmas handled

--- qoqo_qiskit/qoqo_qiskit/backend.py
+++ qoqo_qiskit/qoqo_qiskit/backend.py
@@ -14,18 +14,23 @@ class QoqoQiskitSimulator:
         """Init for Qiskit simulator settings."""
         pass
 
-    def simulate(self, quantum_program: QuantumProgram):
-        """Simulate the QuantumProgram on a Qiskit simulator.
+    def simulate_circuit(self, circuit: Circuit):
+        """Simulate a Circuit on a Qiskit simulator.
+
+        Args:
+            circuit (Circuit): the Circuit to simulate.
+        """
+        qiskit_circuit = to_qiskit_circuit(circuit)
+
+    def simulate_quantum_program(self, quantum_program: QuantumProgram):
+        """Simulate a QuantumProgram on a Qiskit simulator.
 
         Args:
             quantum_program (QuantumProgram): the QuantumProgram to simulate.
         """
-
-        def simulate_circuit(circuit: Circuit):
-            qiskit_circuit = to_qiskit_circuit(circuit)
-
-        constant_circuit_trans = simulate_circuit(quantum_program.measurement.constant_circuit())
+        constant_circuit_trans = self.simulate_circuit(
+            quantum_program.measurement.constant_circuit())
         transformed_circuits = []
         for circ in quantum_program.measurement.circuits():
-            transf_circ = simulate_circuit(circ)
+            transf_circ = self.simulate_circuit(circ)
             transformed_circuits.append(transf_circ)
--- qoqo_qiskit/qoqo_qiskit/conversion.py
+++ qoqo_qiskit/qoqo_qiskit/conversion.py
@@ -21,7 +21,15 @@ def to_qiskit_circuit(circuit: Circuit) -> Tuple[QuantumCircuit, dict]:
     """
     qasm_backend = QasmBackend()
     input_qasm_str = qasm_backend.circuit_to_qasm_str(circuit)
-
     return_circuit = QuantumCircuit().from_qasm_str(input_qasm_str)
 
-    return (return_circuit, {})
+    sim_dict = {}
+    sim_dict["measurements"] = []
+    for op in circuit:  # TODO: PragmaSetStateVector
+        if "PragmaSetNumberOfMeasurements" in op.tags():
+            sim_dict["measurements"].append((op.readout(), op.number_measurements(), {}))
+        if "PragmaRepeatedMeasurement" in op.tags():
+            sim_dict["measurements"].append(
+                (op.readout(), op.number_measurements(), op.qubit_mapping()))
+
+    return (return_circuit, sim_dict)
