eece42af9fc27f79f7b7c6f684b213cdc789b5af
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-02-14 16:21:23 +0000

    simulate_circuit

--- qoqo_qiskit/qoqo_qiskit/backend.py
+++ qoqo_qiskit/qoqo_qiskit/backend.py
@@ -2,7 +2,7 @@
 """Qoqo-qiskit backend for simulation purposes."""
 
 from qoqo import QuantumProgram, Circuit
-# from qiskit import QuantumCircuit
+from qiskit import Aer
 
 from qoqo_qiskit.conversion import to_qiskit_circuit  # type:ignore
 
@@ -10,27 +10,37 @@ from qoqo_qiskit.conversion import to_qiskit_circuit  # type:ignore
 class QoqoQiskitSimulator:
     """Simulate a Qoqo QuantumProgram on a Qiskit simulator."""
 
-    def __init__(self) -> None:
+    def __init__(self, simulator: str = "aer_simulator") -> None:
         """Init for Qiskit simulator settings."""
         pass
 
-    def simulate_circuit(self, circuit: Circuit):
+    def simulate_circuit(self, circuit: Circuit) -> dict[str, int]:
         """Simulate a Circuit on a Qiskit simulator.
 
         Args:
             circuit (Circuit): the Circuit to simulate.
+
+        Returns:
+            dict[str, int]: dict containing, for each qubit, a str indicating its measurement count
         """
         qiskit_circuit = to_qiskit_circuit(circuit)
+        simulator = Aer.get_backend("aer_simulator")
+        result = simulator.run(qiskit_circuit).result()
+        counts = result.get_counts(qiskit_circuit)
 
-    def simulate_quantum_program(self, quantum_program: QuantumProgram):
+        return counts
+
+    def simulate_quantum_program(self, quantum_program: QuantumProgram) -> list[dict[str, int]]:
         """Simulate a QuantumProgram on a Qiskit simulator.
 
         Args:
             quantum_program (QuantumProgram): the QuantumProgram to simulate.
         """
-        constant_circuit_trans = self.simulate_circuit(
+        constant_circuit_count = self.simulate_circuit(
             quantum_program.measurement.constant_circuit())
-        transformed_circuits = []
+        simulated_circuits_counts = []
         for circ in quantum_program.measurement.circuits():
-            transf_circ = self.simulate_circuit(circ)
-            transformed_circuits.append(transf_circ)
+            circ_counts = self.simulate_circuit(circ)
+            simulated_circuits_counts.append(circ_counts)
+
+        
