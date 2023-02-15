560fd31849cf680813675d9565f04a6c917cc91a
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-02-15 15:09:00 +0000

    simulator choice, PragmaSetStateVector support

--- qoqo_qiskit/qoqo_qiskit/backend.py
+++ qoqo_qiskit/qoqo_qiskit/backend.py
@@ -6,6 +6,10 @@ from qiskit import Aer
 
 from qoqo_qiskit.conversion import to_qiskit_circuit  # type:ignore
 
+from typing import Tuple, Union
+
+ALLOWED_PROVIDERS = ["aer_simulator"]
+
 
 class QoqoQiskitSimulator:
     """Simulate a Qoqo QuantumProgram on a Qiskit simulator."""
@@ -16,7 +20,14 @@ class QoqoQiskitSimulator:
         Args:
             simulator (str): String defining which Qiskit simulator to use.
                              Defaults to 'aer_simulator'.
+
+        Raises:
+            ValueError: the selected provider is not allowed.
         """
+        if simulator not in ALLOWED_PROVIDERS:
+            raise ValueError(  # TODO: flake8 says possible sql injection here
+                "Select provider from the following allowed list: " + ', '.join(ALLOWED_PROVIDERS))
+
         self.simulator = simulator
 
     def simulate_circuit(self, circuit: Circuit) -> dict[str, int]:
@@ -26,24 +37,56 @@ class QoqoQiskitSimulator:
             circuit (Circuit): the Circuit to simulate.
 
         Returns:
-            dict[str, int]: dict containing, for each qubit, a str indicating its measurement count
+            dict[str, int]: dict containing, for each qubit, a str indicating its measurement count.
+
+        Raises:
+            ValueError: the Circuit does not contain Measurement operations
         """
-        qiskit_circuit = to_qiskit_circuit(circuit)
+        if not self._are_measurement_operations_in(circuit):
+            raise ValueError(
+                "The Circuit does not contain Measurement operations. Simulation not possible.")
+
+        qiskit_circuit, _ = to_qiskit_circuit(circuit)
+
         simulator = Aer.get_backend(self.simulator)
         result = simulator.run(qiskit_circuit).result()
         counts = result.get_counts(qiskit_circuit)
 
         return counts
 
-    def simulate_quantum_program(self, quantum_program: QuantumProgram) -> list[dict[str, int]]:
-        """Simulate a QuantumProgram on a Qiskit simulator.
+    #         TODO: restructure: build the circuit(s)
+    # def simulate_quantum_program(self, quantum_program: QuantumProgram) -> list[dict[str, int]]:
+    #     """Simulate a QuantumProgram on a Qiskit simulator.
 
-        Args:
-            quantum_program (QuantumProgram): the QuantumProgram to simulate.
-        """
-        constant_circuit_count = self.simulate_circuit(
-            quantum_program.measurement.constant_circuit())
-        simulated_circuits_counts = []
-        for circ in quantum_program.measurement.circuits():
-            circ_counts = self.simulate_circuit(circ)
-            simulated_circuits_counts.append(circ_counts)
+    #     Args:
+    #         quantum_program (QuantumProgram): the QuantumProgram to simulate.
+    #     """
+    #     if not self._are_measurement_operations_in(quantum_program):
+    #         raise ValueError(
+    #             "The QuantumProgram does not contain Measurement operations. Simulation not possible.")
+
+    #     constant_circuit_count = self.simulate_circuit(
+    #         quantum_program.measurement.constant_circuit())
+    #     simulated_circuits_counts = []
+    #     for circ in quantum_program.measurement.circuits():
+    #         circ_counts = self.simulate_circuit(circ)
+    #         simulated_circuits_counts.append(circ_counts)
+
+    def _counts_to_results() -> Tuple[str, int]:
+        pass
+
+    def _are_measurement_operations_in(self, input: Union[Circuit, QuantumProgram]) -> bool:
+        # if isinstance(input, Circuit):
+        for op in input:
+            if "Measurement" in op.tags():
+                return True
+        return False
+        # elif isinstance(input, QuantumProgram):
+        #     for op in input.measurement.constant_circuit():
+        #         if "Measurement" in op.tags():
+        #             return True
+        #     for circ in input.measurement.circuits():
+        #         for op in circ:
+        #             if "Measurement" in op.tags():
+        #                 return True
+        #     return False
--- qoqo_qiskit/qoqo_qiskit/conversion.py
+++ qoqo_qiskit/qoqo_qiskit/conversion.py
@@ -9,27 +9,43 @@ from qoqo_qasm import QasmBackend
 from typing import Tuple
 
 
-def to_qiskit_circuit(circuit: Circuit) -> Tuple[QuantumCircuit, dict]:
+def to_qiskit_circuit(circuit: Circuit) -> Tuple[QuantumCircuit, dict[str, int]]:
     """Applies the qoqo Circuit -> Qiskit QuantumCircuit conversion.
 
     Args:
         circuit (Circuit): the qoqo Circuit to port.
 
     Returns:
-        Tuple[QuantumCircuit, dict]: the equivalent QuantumCircuit and the dict containing
+        Tuple[QuantumCircuit, dict[str, int]]: the equivalent QuantumCircuit and the dict containing
                                      info for Qiskit's simulator.
     """
-    qasm_backend = QasmBackend()
-    input_qasm_str = qasm_backend.circuit_to_qasm_str(circuit)
-    return_circuit = QuantumCircuit().from_qasm_str(input_qasm_str)
-
+    filtered_circuit = Circuit()
     sim_dict = {}
     sim_dict["measurements"] = []
-    for op in circuit:  # TODO: PragmaSetStateVector
-        if "PragmaSetNumberOfMeasurements" in op.tags():
+    initial_statevector = []
+    for op in circuit:
+        if "PragmaSetStateVector" in op.tags():
+            initial_statevector = op.statevector()
+        elif "PragmaSetNumberOfMeasurements" in op.tags():
             sim_dict["measurements"].append((op.readout(), op.number_measurements(), {}))
-        if "PragmaRepeatedMeasurement" in op.tags():
+            filtered_circuit += op
+        elif "PragmaRepeatedMeasurement" in op.tags():
             sim_dict["measurements"].append(
                 (op.readout(), op.number_measurements(), op.qubit_mapping()))
+            filtered_circuit += op
+        else:
+            filtered_circuit += op
+
+    qasm_backend = QasmBackend()
+    input_qasm_str = qasm_backend.circuit_to_qasm_str(filtered_circuit)
+
+    return_circuit = QuantumCircuit()
+    from_qasm_circuit = QuantumCircuit().from_qasm_str(input_qasm_str)
+    if len(initial_statevector) != 0:
+        initial_circuit = QuantumCircuit(from_qasm_circuit.num_qubits, from_qasm_circuit.num_clbits)
+        initial_circuit.initialize(initial_statevector)
+        return_circuit = initial_circuit.compose(from_qasm_circuit)
+    else:
+        return_circuit = from_qasm_circuit
 
     return (return_circuit, sim_dict)
