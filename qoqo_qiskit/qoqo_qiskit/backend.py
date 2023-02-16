fe46323013702db68b37beb631bed1b0975a9d6d
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-02-16 12:00:30 +0000

    temp

--- qoqo_qiskit/qoqo_qiskit/backend.py
+++ qoqo_qiskit/qoqo_qiskit/backend.py
@@ -2,19 +2,20 @@
 """Qoqo-qiskit backend for simulation purposes."""
 
 from qoqo import QuantumProgram, Circuit
-from qiskit import Aer
+from qiskit import Aer, Backend
 
 from qoqo_qiskit.conversion import to_qiskit_circuit  # type:ignore
 
 from typing import Tuple, Union
 
 ALLOWED_PROVIDERS = ["aer_simulator"]
+error_str = f"Select provider from the following allowed list: {ALLOWED_PROVIDERS}"  
 
 
 class QoqoQiskitSimulator:
     """Simulate a Qoqo QuantumProgram on a Qiskit simulator."""
 
-    def __init__(self, simulator: str = "aer_simulator") -> None:
+    def __init__(self, simulator: Backend = None) -> None:
         """Init for Qiskit simulator settings.
 
         Args:
@@ -25,12 +26,11 @@ class QoqoQiskitSimulator:
             ValueError: the selected provider is not allowed.
         """
         if simulator not in ALLOWED_PROVIDERS:
-            raise ValueError(  # TODO: flake8 says possible sql injection here
-                "Select provider from the following allowed list: " + ', '.join(ALLOWED_PROVIDERS))
+            raise ValueError(error_str)
 
-        self.simulator = simulator
+        self.simulator = simulator  # create Aer
 
-    def simulate_circuit(self, circuit: Circuit) -> dict[str, int]:
+    def run_circuit(self, circuit: Circuit) -> dict[str, int]:
         """Simulate a Circuit on a Qiskit simulator.
 
         Args:
@@ -48,30 +48,29 @@ class QoqoQiskitSimulator:
 
         qiskit_circuit, _ = to_qiskit_circuit(circuit)
 
-        simulator = Aer.get_backend(self.simulator)
-        result = simulator.run(qiskit_circuit).result()
+        result = self.simulator.run(qiskit_circuit).result()
         counts = result.get_counts(qiskit_circuit)
 
         return counts
 
     #         TODO: restructure: build the circuit(s)
-    # def simulate_quantum_program(self, quantum_program: QuantumProgram) -> list[dict[str, int]]:
-    #     """Simulate a QuantumProgram on a Qiskit simulator.
+    def run_quantum_program(self, quantum_program: QuantumProgram) -> list[dict[str, int]]:
+        """Simulate a QuantumProgram on a Qiskit simulator.
 
-    #     Args:
-    #         quantum_program (QuantumProgram): the QuantumProgram to simulate.
-    #     """
-    #     if not self._are_measurement_operations_in(quantum_program):
-    #         raise ValueError(
-    #             "The QuantumProgram does not contain Measurement operations.
-    # Simulation not possible.")
+        Args:
+            quantum_program (QuantumProgram): the QuantumProgram to simulate.
+        """
+        if not self._are_measurement_operations_in(quantum_program):
+            raise ValueError(
+                "The QuantumProgram does not contain Measurement operations. Simulation not possible.")
 
-    #     constant_circuit_count = self.simulate_circuit(
-    #         quantum_program.measurement.constant_circuit())
-    #     simulated_circuits_counts = []
-    #     for circ in quantum_program.measurement.circuits():
-    #         circ_counts = self.simulate_circuit(circ)
-    #         simulated_circuits_counts.append(circ_counts)
+        quantum_program.run(self.simulator, )
+
+    def run_measurement_registers():
+        pass
+
+    def run_measurement_registers():
+        pass
 
     def _counts_to_results() -> Tuple[str, int]:
         pass
