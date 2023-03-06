29633b72f835edc42f46e76eaefaacdf2abd4af0
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-03-06 14:10:08 +0000

    missing init

---
+++ qoqo_qiskit/qoqo_qiskit/__init__.py
@@ -0,0 +1,18 @@
+"""Qoqo-qiskit backend package for compatibility and simulation purposes.
+
+Translates qoqo operations and circuits to qiskit equivalents.
+
+.. autosummary::
+    :toctree: generated/
+
+    to_qiskit_circuit
+    QoqoQiskitSimulator
+
+"""
+# Copyright © 2023 HQS Quantum Simulations GmbH.
+
+
+from qoqo_qiskit.backend import QoqoQiskitSimulator
+from qoqo_qiskit.interface import to_qiskit_circuit
+
+print("Copyright © 2023 HQS Quantum Simulations GmbH.")
