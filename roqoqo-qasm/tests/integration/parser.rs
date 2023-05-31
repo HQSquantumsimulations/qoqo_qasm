c6b8dbcd2b3b88fe3464ec4e9ce9febbee6c6b8d
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-05-31 08:25:10 +0000

    all gates test

--- roqoqo-qasm/tests/integration/parser.rs
+++ roqoqo-qasm/tests/integration/parser.rs
@@ -36,3 +36,51 @@ fn test_basic_file() {
 
     assert_eq!(circuit_from_file, circuit_qoqo);
 }
+
+/// Test all implemented gates
+#[test]
+fn test_gates() {
+    let file = File::open(std::env::current_dir().unwrap().join("tests/gates.qasm")).unwrap();
+
+    let circuit_from_file = qasm_file_to_circuit(file).unwrap();
+
+    let mut circuit_qoqo = Circuit::new();
+    circuit_qoqo += DefinitionBit::new("c".into(), 2, true);
+    circuit_qoqo += RotateZ::new(0, 0.2.into());
+    circuit_qoqo += RotateY::new(1, 0.3.into());
+    circuit_qoqo += RotateX::new(2, 2.1.into());
+    circuit_qoqo += Hadamard::new(0);
+    circuit_qoqo += PauliX::new(2);
+    circuit_qoqo += PauliY::new(1);
+    circuit_qoqo += PauliZ::new(0);
+    circuit_qoqo += SGate::new(0);
+    circuit_qoqo += TGate::new(1);
+    circuit_qoqo += PhaseShiftState1::new(2, 0.6.into());
+    circuit_qoqo += SqrtPauliX::new(1);
+    circuit_qoqo += InvSqrtPauliX::new(0);
+    circuit_qoqo += CNOT::new(0, 1);
+    circuit_qoqo += MolmerSorensenXX::new(1, 2);
+    circuit_qoqo += VariableMSXX::new(0, 2, 0.7.into());
+    circuit_qoqo += ControlledPauliY::new(0, 1);
+    circuit_qoqo += ControlledPauliZ::new(1, 2);
+    circuit_qoqo += ControlledPhaseShift::new(0, 2, 0.5.into());
+    circuit_qoqo += SWAP::new(1, 2);
+    circuit_qoqo += ISwap::new(0, 1);
+    circuit_qoqo += SqrtISwap::new(1, 2);
+    circuit_qoqo += InvSqrtISwap::new(0, 2);
+    circuit_qoqo += FSwap::new(0, 1);
+    circuit_qoqo += Fsim::new(1, 2, 0.1.into(), 0.2.into(), 0.3.into());
+    circuit_qoqo += Qsim::new(0, 2, 1.0.into(), 1.1.into(), 1.2.into());
+    circuit_qoqo += PMInteraction::new(1, 2, 0.8.into());
+    circuit_qoqo += GivensRotation::new(1, 2, 0.2.into(), 0.9.into());
+    circuit_qoqo += GivensRotationLittleEndian::new(0, 2, 0.4.into(), 0.8.into());
+    circuit_qoqo += XY::new(1, 2, 0.5.into());
+    circuit_qoqo += SpinInteraction::new(0, 2, 0.5.into(), 0.6.into(), 0.7.into());
+    circuit_qoqo += RotateXY::new(0, 0.3.into(), 0.9.into());
+    circuit_qoqo += PhaseShiftedControlledZ::new(0, 2, 0.3.into());
+    circuit_qoqo += PhaseShiftedControlledPhase::new(0, 1, 1.4.into(), 1.9.into());
+    circuit_qoqo += PragmaActiveReset::new(1);
+    circuit_qoqo += MeasureQubit::new(0, "c".into(), 0);
+    
+    assert_eq!(circuit_from_file, circuit_qoqo);
+}
